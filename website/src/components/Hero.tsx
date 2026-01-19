202c88875b3a099629170dbe4ab8f3e7f685c576
Javier-GarciaP <josejaviergarciap123@gmail.com>
2026-01-19 10:10:24 -0400

    Estado 😁: Ajuste en el Hero

--- website/src/components/Hero.tsx
+++ website/src/components/Hero.tsx
@@ -9,7 +9,7 @@ export const Hero = () => {
             </div>
             <h1 className="text-gray-800 font-mono text-6xl md:text-8xl font-black mb-4">Biper</h1>
             <p className="text-gray-600 text-lg md:text-xl max-w-lg">Gestor de notificaciones push para aplicaciones react</p>
-            <div className="flex flex-col sm:flex-row gap-4 mt-8 w-full sm:w-auto">
+            <div className="flex flex-col md:flex-row gap-4 mt-8 w-full sm:w-auto">
                 <button className="bg-gray-800 text-white px-8 py-3 rounded-xl font-bold transition-all duration-300 hover:scale-105 hover:shadow-lg hover:cursor-pointer active:scale-95" onClick={() => toast.success("¡Biper es increible! 🔥🔥🔥🔥", { position: "top-center" })}>Probar Biper</button>
                 <button className="bg-white text-gray-800 border-2 border-gray-800 px-8 py-3 rounded-xl font-bold transition-all duration-300 hover:scale-105 hover:bg-gray-50 hover:cursor-pointer active:scale-95">Documentación</button>
             </div>
